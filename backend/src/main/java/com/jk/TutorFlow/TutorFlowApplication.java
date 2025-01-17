8c244e5812d83ea2bec3740e811f5ae5ba39b082
Hortensjaa <lilka305@gmail.com>
2025-01-17 15:24:16 +0100

    first commit

---
+++ backend/src/main/java/com/jk/TutorFlow/TutorFlowApplication.java
@@ -0,0 +1,13 @@
+package com.jk.TutorFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorFlowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jk/TutorFlow/TutorFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.TutorFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
